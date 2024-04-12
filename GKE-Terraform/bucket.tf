f6e444a31dcea1c1463d76f8fd3c407417258eb7
Romu Tiwari <tromu98@gmail.com>
2024-04-12 19:19:54 +0530

    Removed Bucket Config

--- GKE-Terraform/bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "google_storage_bucket" "backend_bucket" {
-  name          = "terraform-boardgame-bucket-tfstate"
-  force_destroy = false
-  location      = "US"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-}
